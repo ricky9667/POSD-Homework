d13b7e0bf76b21d84493f8736d9fe2f88255b419
Ricky Hu <t108820008@ntut.org.tw>
2022-09-13 23:40:43 +0800

    HW1 test: add PointTest.testOperatorEqual

--- test/ut_point.h
+++ test/ut_point.h
@@ -6,3 +6,13 @@ TEST(PointTest, testPointGetters)
     ASSERT_EQ(-4.586, p.getX());
     ASSERT_EQ(-3.471, p.getY());
 }
+
+TEST(PointTest, testOperatorEqual)
+{
+    Point p(-4.586, -3.471);
+    Point p1(-4.586, -3.471);
+    Point p2(4.586, 3.471);
+
+    ASSERT_TRUE(p == p1);
+    ASSERT_FALSE(p == p2);
+}
