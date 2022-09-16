4827013bc4e66c8c060cc115ae59f8ae9f11daf0
Ricky Hu <t108820008@ntut.org.tw>
2022-09-16 17:48:17 +0800

    HW1 test: add RectangleTest.testRectangleGetters

--- test/ut_rectangle.h
+++ test/ut_rectangle.h
@@ -2,10 +2,12 @@
 #include "../src/two_dimensional_vector.h"
 #include "../src/rectangle.h"
 
-TEST(RectangleTest, test)
+TEST(RectangleTest, testRectangleGetters)
 {
-    Rectangle *rectangle = new Rectangle(
-        new TwoDimensionalVector(new Point(-2, 1), new Point(1.5, 0.47)),
-        new TwoDimensionalVector(new Point(-2, 1), new Point(-1.47, 4.5))
-    );
+    TwoDimensionalVector *lengthVector = new TwoDimensionalVector(new Point(-2, 1), new Point(1.5, 0.47));
+    TwoDimensionalVector *widthVector = new TwoDimensionalVector(new Point(-2, 1), new Point(-1.47, 4.5));
+    Rectangle *rectangle = new Rectangle(lengthVector, widthVector);
+
+    ASSERT_EQ(lengthVector->length(), rectangle->length());
+    ASSERT_EQ(widthVector->length(), rectangle->width());
 }
