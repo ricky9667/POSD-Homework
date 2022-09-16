ca3df4a08511b02cfa5f9ae40e18ec5c7ecbc46c
Ricky Hu <t108820008@ntut.org.tw>
2022-09-16 16:47:57 +0800

    HW1 test: add TriangleTest.testTriangleArea

--- test/ut_triangle.h
+++ test/ut_triangle.h
@@ -2,10 +2,12 @@
 #include "../src/two_dimensional_vector.h"
 #include "../src/triangle.h"
 
-TEST(TriangleTest, test)
+TEST(TriangleTest, testTriangleArea)
 {
     Triangle *triangle = new Triangle(
         new TwoDimensionalVector(new Point(0, 0), new Point(3, 0)),
         new TwoDimensionalVector(new Point(3, 4), new Point(3, 0))
     );
+
+    ASSERT_NEAR(6, triangle->area(), 0.001);
 }
