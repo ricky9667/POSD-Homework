7544668b4cf9b2d4cf721172a76f7157eb6af044
Ricky Hu <t108820008@ntut.org.tw>
2022-10-21 16:50:43 +0800

    fix: move iterator factory implementations to .cpp file

--- src/iterator/factory/bfs_iterator_factory.h
+++ src/iterator/factory/bfs_iterator_factory.h
@@ -1,18 +1,9 @@
 #pragma once
 
 #include "./iterator_factory.h"
-#include "../null_iterator.h"
-#include "../bfs_compound_iterator.h"
 
 class BFSIteratorFactory : public IteratorFactory
 {
-    Iterator *createIterator()
-    {
-        return new NullIterator();
-    }
-
-    Iterator *createIterator(std::list<Shape *>::const_iterator begin, std::list<Shape *>::const_iterator end)
-    {
-        return new BFSCompoundIterator(begin, end);
-    }
+    Iterator *createIterator();
+    Iterator *createIterator(std::list<Shape *>::const_iterator begin, std::list<Shape *>::const_iterator end);
 };
--- src/iterator/factory/dfs_iterator_factory.h
+++ src/iterator/factory/dfs_iterator_factory.h
@@ -1,18 +1,9 @@
 #pragma once
 
 #include "./iterator_factory.h"
-#include "../null_iterator.h"
-#include "../dfs_compound_iterator.h"
 
 class DFSIteratorFactory : public IteratorFactory
 {
-    Iterator *createIterator()
-    {
-        return new NullIterator();
-    }
-
-    Iterator *createIterator(std::list<Shape *>::const_iterator begin, std::list<Shape *>::const_iterator end)
-    {
-        return new DFSCompoundIterator(begin, end);
-    }
+    Iterator *createIterator();
+    Iterator *createIterator(std::list<Shape *>::const_iterator begin, std::list<Shape *>::const_iterator end);
 };
