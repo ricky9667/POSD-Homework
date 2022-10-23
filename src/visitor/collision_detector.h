557d6a1e9a3daa5c107d6b6ebaf2a91c3f0becd0
Ricky Hu <t108820008@ntut.org.tw>
2022-10-23 21:07:34 +0800

    feat: implement CollisionDetector methods

--- src/visitor/collision_detector.h
+++ src/visitor/collision_detector.h
@@ -7,7 +7,7 @@
 #include "../rectangle.h"
 #include "../compound_shape.h"
 #include "../bounding_box.h"
-#include "../iterator/factory/bfs_iterator_factory.h"
+#include "../iterator/factory/list_iterator_factory.h"
 
 class CollisionDetector : public ShapeVisitor
 {
@@ -21,13 +21,38 @@ public:
         _targetBoundingBox = new BoundingBox(shape->getPoints());
     }
 
-    void visitCircle(Circle *circle) override {}
+    void visitCircle(Circle *circle) override
+    {
+        BoundingBox *circleBoundingBox = new BoundingBox(circle->getPoints());
+        if (_targetBoundingBox->collide(circleBoundingBox))
+            _collideResult.push_back(circle);
+    }
 
-    void visitTriangle(Triangle *triangle) override {}
+    void visitTriangle(Triangle *triangle) override
+    {
+        BoundingBox *triangleBoundingBox = new BoundingBox(triangle->getPoints());
+        if (_targetBoundingBox->collide(triangleBoundingBox))
+            _collideResult.push_back(triangle);
+    }
 
-    void visitRectangle(Rectangle *rectangle) override {}
+    void visitRectangle(Rectangle *rectangle) override
+    {
+        BoundingBox *rectangleBoundingBox = new BoundingBox(rectangle->getPoints());
+        if (_targetBoundingBox->collide(rectangleBoundingBox))
+            _collideResult.push_back(rectangle);
+    }
 
-    void visitCompoundShape(CompoundShape *compoundShape) override {}
+    void visitCompoundShape(CompoundShape *compoundShape) override
+    {
+        Iterator *iterator = compoundShape->createIterator(new ListIteratorFactory());
+        for (; !iterator->isDone(); iterator->next())
+        {
+            Shape *currentShape = iterator->currentItem();
+            BoundingBox *boundingBox = new BoundingBox(currentShape->getPoints());
+            if (_targetBoundingBox->collide(boundingBox))
+                _collideResult.push_back(currentShape);
+        }
+    }
 
     std::vector<Shape *> collidedShapes() const
     {
