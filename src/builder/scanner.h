8c422c6f7eabbd58a4e9b2502a8d078b0c4afd8e
Ricky Hu <t108820008@ntut.org.tw>
2022-11-25 16:06:07 +0800

    feat: implement Scanner and pass test

---
+++ src/builder/scanner.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <string>
+
+class Scanner
+{
+private:
+    std::string _input;
+    std::string::size_type _pos = 0;
+    const std::vector<std::string> _tokens = {
+        "Circle",
+        "Rectangle",
+        "Triangle",
+        "CompoundShape",
+        "Vector",
+        "(",
+        ")",
+        ","};
+
+    void _skipWhiteSpace()
+    {
+        while (_input[_pos] == ' ' || _input[_pos] == '\n')
+            _pos++;
+    }
+
+    bool _isNumber(char c)
+    {
+        return c >= '0' && c <= '9';
+    }
+
+public:
+    Scanner(std::string input = "") : _input{input} {}
+
+    std::string next()
+    {
+        _skipWhiteSpace();
+
+        for (std::string token : _tokens)
+        {
+            if (_input.compare(_pos, token.length(), token) == 0)
+            {
+                _pos += token.length();
+                return token;
+            }
+        }
+
+        // token not found
+        return "";
+    }
+
+    double nextDouble()
+    {
+        std::string s = "";
+        _skipWhiteSpace();
+
+        if (_input[_pos] == '-')
+            s += _input[_pos++];
+        
+        while (_isNumber(_input[_pos]))
+            s += _input[_pos++];
+
+        if (_input[_pos] == '.')
+            s += _input[_pos++];
+
+        while (_isNumber(_input[_pos]))
+            s += _input[_pos++];
+
+        return std::stod(s);
+    }
+
+    bool isDone()
+    {
+        _skipWhiteSpace();
+        return _pos == _input.length();
+    }
+};
