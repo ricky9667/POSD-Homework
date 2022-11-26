2b67729a0af12744de81fb38d4ed0da02eee0cfc
Ricky Hu <t108820008@ntut.org.tw>
2022-11-27 00:29:21 +0800

    test: refactor tests

--- test/builder/ut_shape_parser.h
+++ test/builder/ut_shape_parser.h
@@ -5,11 +5,10 @@
 class ShapeParserTest : public ::testing::Test
 {
 protected:
-    std::string testInput1 = "Circle (Vector ((-4.28, 0.26), (-4.83, 0.73)))";
+    std::string circleInput = "Circle (Vector ((-4.28, 0.26), (-4.83, 0.73)))";
     std::string triangleInput = "Triangle (Vector ((0.00, 0.00), (3.00, 0.00)), Vector ((3.00, 4.00), (3.00, 0.00)))";
-    std::string testInput4 = "Rectangle (Vector ((-2.00, 1.00), (1.50, 0.47)), Vector ((-2.00, 1.00), (-1.47, 4.50)))";
-    std::string testInput2 = "CompoundShape (Triangle (Vector ((0.00, 0.00), (2.00, 0.00)), Vector ((0.00, 0.00), (8.00, 2.00))), Rectangle (Vector ((0.00, 0.00), (2.00, 0.00)), Vector ((0.00, 0.00), (0.00, 8.00))))";
-    std::string testInput3 = testInput1 + ",\n" + testInput2;
+    std::string rectangleInput = "Rectangle (Vector ((-2.00, 1.00), (1.50, 0.47)), Vector ((-2.00, 1.00), (-1.47, 4.50)))";
+    std::string compoundShapeInput = "CompoundShape (Triangle (Vector ((0.00, 0.00), (2.00, 0.00)), Vector ((0.00, 0.00), (8.00, 2.00))), Rectangle (Vector ((0.00, 0.00), (2.00, 0.00)), Vector ((0.00, 0.00), (0.00, 8.00))))";
 
     void SetUp() {}
 
@@ -18,12 +17,12 @@ protected:
 
 TEST_F(ShapeParserTest, ParseCircleShouldWorkExpectedly)
 {
-    ShapeParser parser(testInput1);
+    ShapeParser parser(circleInput);
     parser.parse();
     std::vector<Shape *> result = parser.getResult();
 
     ASSERT_EQ(1, result.size());
-    ASSERT_EQ(testInput1, result.at(0)->info());
+    ASSERT_EQ(circleInput, result.at(0)->info());
 }
 
 TEST_F(ShapeParserTest, ParseTriangleShouldWorkExpectedly)
@@ -38,31 +37,32 @@ TEST_F(ShapeParserTest, ParseTriangleShouldWorkExpectedly)
 
 TEST_F(ShapeParserTest, ParseRectangleShouldWorkExpectedly)
 {
-    ShapeParser parser(testInput4);
+    ShapeParser parser(rectangleInput);
     parser.parse();
     std::vector<Shape *> result = parser.getResult();
 
     ASSERT_EQ(1, result.size());
-    ASSERT_EQ(testInput4, result.at(0)->info());
+    ASSERT_EQ(rectangleInput, result.at(0)->info());
 }
 
 TEST_F(ShapeParserTest, ParseCompoundShapeShouldWorkExpectedly)
 {
-    ShapeParser parser(testInput2);
+    ShapeParser parser(compoundShapeInput);
     parser.parse();
     std::vector<Shape *> result = parser.getResult();
 
     ASSERT_EQ(1, result.size());
-    ASSERT_EQ(testInput2, result.at(0)->info());
+    ASSERT_EQ(compoundShapeInput, result.at(0)->info());
 }
 
 TEST_F(ShapeParserTest, ParseMultiplueShapesShouldWorkExpectedly)
 {
-    ShapeParser parser(testInput3);
+    std::string input = circleInput + ",\n" + compoundShapeInput;
+    ShapeParser parser(input);
     parser.parse();
     std::vector<Shape *> result = parser.getResult();
 
     ASSERT_EQ(2, result.size());
-    ASSERT_EQ(testInput1, result.at(0)->info());
-    ASSERT_EQ(testInput2, result.at(1)->info());
+    ASSERT_EQ(circleInput, result.at(0)->info());
+    ASSERT_EQ(compoundShapeInput, result.at(1)->info());
 }
