4fdb4f681156f3ecfa267d812ba28f6fd80254f5
Ricky Hu <t108820008@ntut.org.tw>
2022-11-27 00:22:43 +0800

    test: add tests for parsing Rectangle

--- test/builder/ut_shape_parser.h
+++ test/builder/ut_shape_parser.h
@@ -8,6 +8,7 @@ protected:
     std::string testInput1 = "Circle (Vector ((-4.28, 0.26), (-4.83, 0.73)))";
     std::string testInput2 = "CompoundShape (Triangle (Vector ((0.00, 0.00), (2.00, 0.00)), Vector ((0.00, 0.00), (8.00, 2.00))), Rectangle (Vector ((0.00, 0.00), (2.00, 0.00)), Vector ((0.00, 0.00), (0.00, 8.00))))";
     std::string testInput3 = testInput1 + ",\n" + testInput2;
+    std::string testInput4 = "Rectangle (Vector ((-2.00, 1.00), (1.50, 0.47)), Vector ((-2.00, 1.00), (-1.47, 4.50)))";
 
     void SetUp() {}
 
@@ -22,6 +23,16 @@ TEST_F(ShapeParserTest, ParseCircleShouldWorkExpectedly)
 
     ASSERT_EQ(1, result.size());
     ASSERT_EQ(testInput1, result.at(0)->info());
+}
+
+TEST_F(ShapeParserTest, ParseRectangleShouldWorkExpectedly)
+{
+    ShapeParser parser(testInput4);
+    parser.parse();
+    std::vector<Shape *> result = parser.getResult();
+
+    ASSERT_EQ(1, result.size());
+    ASSERT_EQ(testInput4, result.at(0)->info());
 }
 
 TEST_F(ShapeParserTest, ParseCompoundShapeShouldWorkExpectedly)
